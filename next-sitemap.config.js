b4da53828bf68ca27db34355a6a9b1f517001c73
Sohan Gamalath <gamalathsohan@gmail.com>
2025-03-04 14:41:01 +0530

    feat:implement sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://ezbooq.com", // Change this to your actual domain
+  generateRobotsTxt: true, // Generate a robots.txt file
+  generateIndexSitemap: false, // Optional: Set to true if needed
+};
